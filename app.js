11232daa042a6eb83048963fc4bfafe6669e99b3
Cecile-29 <cecile.auger.pro@gmail.com>
2021-04-07 21:02:13 +0200

    push exercice après correction et commentaires

---
+++ app.js
@@ -0,0 +1,8 @@
+function addTableContent(){
+  var tableau = ['HTML', 'CSS', 'JS', 'BOOSTRAP'];
+  alert(document.getElementById ('d1').innerHTML = tableau);
+}
+function displayTableContent(){
+  var tableau = ['HTML', 'CSS', 'JS', 'BOOSTRAP'];
+  alert(document.getElementById ('d1').innerHTML = tableau);
+}
