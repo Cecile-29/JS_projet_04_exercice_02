021e2d4bfa9002611756836c1f09bd08d1288440
Cecile-29 <cecile.auger.pro@gmail.com>
2021-04-13 10:30:41 +0200

    push exercice après correction et commentaires

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //	la fonction pour confirmation d'ajout affiché dans une modale
-  function addTableContent() {
-    var addText = document.getElementById('inputText').value;
-      alert(addText + ' a été ajouté au tableau');
-  }
+function addTableContent() {
+  var addText = document.getElementById('inputText').value;
+  alert(addText + ' a été ajouté au tableau');
+}
 // déclaration d'un tableau vide pour recevoir la chaîne de caractères de l'input
 var monTableau = [];
 //fonction appelée au click du bouton voir
@@ -17,12 +17,21 @@ function addAndDisplayContent() {
   La vérification que i est strictement inférieur à la longueur avec La propriété .length,
   puis l'incrémentation i de 1, ce qui sera fait à chaque passage dans la boucle.*/
   for (i = 0; i < monTableau.length; i++) {
-  /* dans le parcours du tableau par l'instruction for, on concatène toutes les valeurs
-  du tableau avec un caractère "," pour pouvoir afficher les valeurs de l'input passées
-  en paramètres séparées par une virgule dans une modale après la boucle for.
-  */
+    /* dans le parcours du tableau par l'instruction for, on concatène toutes les valeurs
+    du tableau avec un caractère "," pour pouvoir afficher les valeurs de l'input passées
+    en paramètres séparées par une virgule dans une modale après la boucle for.
+    */
     (monTableau[i] + ',');
   }
   // display du tableau dans une modale et dans une div
   alert(document.getElementById('divText').innerHTML = monTableau);
 }
+/* *************************Correction Katherine****************/
+
+// let animals = ['Bear', 'Lion', 'cat', 'monkey'];
+// function addMoreAnimals() {
+//   console.log();
+//   let newDatas= document.getElementById('input').value;
+//   animals.unshift(newDatas);
+//   alert(animals + ' a été ajouté au tableau !');
+// }
